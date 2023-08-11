f4f6dacda281e0d4f9a27e860f99a6dbedec6a7a
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-11 21:12:19 +0100

    Digital numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Digital numbers of base 10 begining with 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i == 0;
+
+	do
+	{
+		printf("%d", i);
+		i++;
+	}
+	while (i < 10);
+	return (0);
+}
