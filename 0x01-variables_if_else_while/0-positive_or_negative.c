e6b63170a1af93578b0e7019cc529dcc9268beb5
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-11 17:03:21 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	}
 	else if(n==0)
 	{
-		printf("%d is zero");
+		printf("%d is zero",n);
 	}
 	else if(n<0)
 	{
-		printf("%d is negative");
+		printf("%d is negative",n);
 	}
 	return 0;
 }
