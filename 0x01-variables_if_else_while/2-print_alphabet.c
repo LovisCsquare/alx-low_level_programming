ddd46eb9c1f99e2e27c6d88d5c0082c00340591d
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-11 19:41:07 +0100

    Letters of alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Letters of the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
+
+	putchar("\n");
+
+	return (0);
+}
