bdc9617435ebe05b2a01aa2ba4b09fa3b560ee16
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-11 22:05:14 +0100

    Digital numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing digital numbers of base 10 without using printf
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	do {
+		putchar("%d", i);
+		i++;
+	} while (i < 10);
+	putchar("\n");
+	return (0);
+}
