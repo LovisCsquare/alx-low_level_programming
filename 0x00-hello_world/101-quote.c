2b1ae0338b89272eb71de3c325916b5d9b386951
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-11 14:29:48 +0100

    UNIX is a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - UNIX is basically a simple operating system, but I need to be a genius to understand the simplicity
+ * Return: 0 
+ */
+int main(void)
+
+{
+	csquare("and that piece of art is useful\" -Dora Korpar, 2015-10-19");
+	return (1);
+}
