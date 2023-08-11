245de3bad75350993f6a5022f70b19130271b09f
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-11 14:39:46 +0100

    UNIX is the best

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - UNIX is basically a simple operating system, but I need to be a genius to understand the simplicity
- * Return: 0 
+ * Return: 1 
  */
 int main(void)
 
 {
-	csquare("and that piece of art is useful\" -Dora Korpar, 2015-10-19");
+	write("and that piece of art is useful\" -Dora Korpar, 2015-10-19");
 	return (1);
 }
