b7a3d4459ec64202591cecf31f23acfeb8ff4167
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-16 00:50:12 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('r');
+	_putchar('h');
+	_putchar('a');
+	_putchar('c');
+	_putchar('u');
+	_putchar('t');
+	_putchar('p');
+	return (0);
+}
