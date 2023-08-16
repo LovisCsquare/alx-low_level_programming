355159614769be7fe19dbcc7c68e29dfc574887b
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-16 01:07:50 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - function that prints letters of the alphabets in lowercase
+ * Return: 0
+ * 1-alphabet.c
+ */
+void print_alphabet(void)
+{
+	int letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar (letter);
+	}
+	{
+		_putchar (letter);
+	}
+	_putchar (10);
+	return (0);
+
+}
