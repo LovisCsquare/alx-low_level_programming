454cb2c922d29db31b4773d0cfef37440a238dd2
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-16 01:19:50 +0100

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,13 @@
  * main - function that prints letters of the alphabets in lowercase
  * Return: 0
  */
+
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }
