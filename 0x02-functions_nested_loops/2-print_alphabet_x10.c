75bf8b755bf1d018788dca6a5c267d42ae292111
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-16 02:05:59 +0100

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	int letter ;
+	char letter;
+	int count;
 
-	while (count++ <= 9)
+	count = 1;
+	while (count <= 10);
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
-			_putchar(10);
+		}
+		_putchar('\n');
+		count++
 	}
 }
+
