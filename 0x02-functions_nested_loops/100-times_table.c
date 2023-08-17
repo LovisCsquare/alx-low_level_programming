bcdffd76d8950ea6e136e4713fc0146c6f3cf7b9
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-17 02:06:43 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,6 @@ void print_times_table(int n)
 	int i, j, k;
 
 	if (n >= 0 && n <= 15)
-	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -43,5 +42,4 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
-	}
 }
