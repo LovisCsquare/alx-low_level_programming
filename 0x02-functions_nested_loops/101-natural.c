0f6bec902f9937a3e34948d5317c3128bd6c350b
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-17 02:55:53 +0100

    Natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.>
+/**
+ * main - sum of multiples
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int sum_of_3, sum_of_5, sum;
+
+	sum_of_3 = 0;
+	sum_of_5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			sum_of_3 = sum_of_3 + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			 sum_of_5 =  sum_of_5 + i;
+		}
+	}
+	sum =  sum_of_5 +  sum_of_3;
+	printf("%lu\n", sum);
+	return (0);
+}
