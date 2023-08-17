691e8e67181f4edad1e1e755880ad2ced28165bf
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-17 02:14:07 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,6 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
 					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)
 				{
@@ -41,6 +40,7 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
