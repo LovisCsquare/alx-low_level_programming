b72eede173017e36565b77616fe39d6f0b662e22
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-17 03:30:28 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Fabonnaci sequence
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int n, j = 1; k = 2; sum = 0;
+
+	for (i = 1; i <= 33; ++1)
+	{
+		if ((j < 4000000) && (j % 2) == 0)
+		{
+			sum =+ j; 
+		}
+		n = j + k;
+		j = k;
+		k = n;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
