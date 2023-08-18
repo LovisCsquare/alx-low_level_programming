7c16dad1c09305871e752cbc36a7d7338dc98a84
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-19 00:15:23 +0100

    Straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,13 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} 
-	else							
+	}
+	else
 	{
-
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
