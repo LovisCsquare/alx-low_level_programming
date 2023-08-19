eb4dc4513480e416db1d428b258f51c21dc45cbb
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-19 01:55:14 +0100

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Function that determines positive and negative integer
- *
+ * Return: True
  */
 int main(void)
 {
@@ -10,6 +10,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
