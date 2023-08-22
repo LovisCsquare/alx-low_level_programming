efa539c10d00e1186b4056e9b0f9a5d7b0c2f400
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-22 22:16:17 +0100

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: 0
+ */
+void swap_-int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
