eaacb049c3268dec991c8bfa8e4651075c69f8d3
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-22 22:40:38 +0100

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,18 @@
-#include "main.h"
+trlen - returns the length of a string
+ * @s: string
+ *  *Return: returns lenght;
+ *   */
+ int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+	
+	return (inc);
+}
+
+include "main.h"
 /**
  * print_rev - prints a string in reverse
  * @s: string to print
