e92b279e732d60324e8d40cabaadc4d011afc96f
LovisCsquare <clovisfuh1996@gmail.com>
2023-08-31 03:05:46 +0100

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,8 +11,13 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	if (*s == 0)
+	{
 		return (1);
+	}
+	else
+	{
 		return (check_pal(s, 0, _strlen_recursion(s)));
+	}
 }
 
 /**
@@ -23,8 +28,13 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		return (0);
+	}
+	else
+	{
 		return (1 + _strlen_recursion(s + 1));
+	}
 }
 
 /**
@@ -37,8 +47,15 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
+	{
 		return (0);
-	if (i >= len)
+	}
+	else if (i >= len)
+	{
 		return (1);
+	}
+	else
+	{
 		return (check_pal(s, i + 1, len - 1));
+	}
 }
